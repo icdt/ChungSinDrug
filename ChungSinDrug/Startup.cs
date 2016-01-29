bbdd4221cc34ebce597012f7cfa49db85fc15ac0
istale <iscarryon@gmail.com>
2016-01-29 13:58:34 +0800

    Initial Commit

---
+++ ChungSinDrug/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChungSinDrug.Startup))]
+namespace ChungSinDrug
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
